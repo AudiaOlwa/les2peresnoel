13fb3ceb3d6f7678dba3df88f6ae46cc05604a26
kaisermyung <myunggodwin@gmail.com>
2024-09-24 14:12:34 +0100

    email verification optionnal

--- config/settings.py
+++ config/settings.py
@@ -403,7 +403,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 # https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+# ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_ADAPTER = "les2peresnoel.users.adapters.AccountAdapter"
 # https://docs.allauth.org/en/latest/account/forms.html
